9502c344523f830f7ef9223ad86e2691f5afdfb0
Harini Parsha(UST <284699@ust.com>
2026-01-07 23:59:27 +0530

    mapped flat files parsers to registry

--- app/core/parsers/__init__.py
+++ app/core/parsers/__init__.py
@@ -1,6 +1,5 @@
 """Parser module for COBOL, PL/I and related file types."""
 
-
 from app.db.enums import SourceFileType
 from app.core.parsers.base import BaseParser
 from app.core.parsers.cobol_parser import CobolParser
@@ -12,6 +11,7 @@ from app.core.parsers.jcl_parser import JCLParser
 from app.core.parsers.ca7_parser import CA7Parser
 from app.core.parsers.bind_parser import BindParser
 
+from app.core.parsers.flatfile_parser import CSVParser, FixedLengthParser
 
 # Registry mapping file types to parser classes
 PARSER_REGISTRY: dict[str, type[BaseParser]] = {
@@ -23,13 +23,15 @@ PARSER_REGISTRY: dict[str, type[BaseParser]] = {
     SourceFileType.JCL.value: JCLParser,
     SourceFileType.CA7.value: CA7Parser,
     SourceFileType.BIND.value: BindParser
+    SourceFileType.CSV.value: CSVParser,
+    SourceFileType.FIXED_LENGTH.value: FixedLengthParser,
 }
 
 def get_parser(file_type: str) -> BaseParser:
     """Get a parser instance for the given file type.
     
     Args:
-        file_type: Type of file to parse (e.g., "cobol", "pli")
+        file_type: Type of file to parse (e.g., "cobol", "pli", "csv", "fixed_length")
         
     Returns:
         Parser instance for the file type
@@ -51,7 +53,8 @@ __all__ = [
     "CopybookParser",
     "PLIParser",
     "PLICopybookParser",
-    "DclgenParser", 
+    "CSVParser",
+    "FixedLengthParser",
     "SourceFileType",
     "PARSER_REGISTRY",
     "get_parser",
